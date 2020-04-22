9ff6f79e7fbffdb909a0371102e5a57c14df63be
Sarathe <Kritika.Sarathe@team.neustar>
2020-04-22 22:33:50 +0530

    Updates

---
+++ precache-manifest.76e68243750248aa194a473a54bbef5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24a3cd95741bff824f3f8f19122118a",
+    "url": "/sarathe-kritika/react_router/index.html"
+  },
+  {
+    "revision": "7fe6403147d6e3902fbc",
+    "url": "/sarathe-kritika/react_router/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "eff59dc79a2d35128b70",
+    "url": "/sarathe-kritika/react_router/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7fe6403147d6e3902fbc",
+    "url": "/sarathe-kritika/react_router/static/js/2.44185876.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sarathe-kritika/react_router/static/js/2.44185876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff59dc79a2d35128b70",
+    "url": "/sarathe-kritika/react_router/static/js/main.c411bef0.chunk.js"
+  },
+  {
+    "revision": "97cdb824d18e934da2dd",
+    "url": "/sarathe-kritika/react_router/static/js/runtime-main.ad7dc203.js"
+  }
+]);
